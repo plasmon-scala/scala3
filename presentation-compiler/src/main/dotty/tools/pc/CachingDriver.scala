29355c82a3cd053300ea51db9d5028a9ab335fa1
Alex Archambault <alexandre.archambault@gmail.com>
2026-02-02 20:04:53 +0100

    Clean-up output

--- presentation-compiler/src/main/dotty/tools/pc/CachingDriver.scala
+++ presentation-compiler/src/main/dotty/tools/pc/CachingDriver.scala
@@ -57,8 +57,6 @@ class CachingDriver(
   private var lastCompiledURI: URI = uninitialized
   private var previousDiags = List.empty[Diagnostic]
 
-  private var printedCp = false
-
   override protected def initCtx: Context = {
     val baseCtx: ContextBase = new ContextBase { baseCtx0 =>
       override protected def newPlatform(using Context): Platform = {
@@ -199,13 +197,7 @@ class CachingDriver(
                 }
 
               val cp = super.classPath
-              val processedCp = process(cp).getOrElse(AggregateClassPath(Nil))
-              if (!printedCp) {
-                System.err.println("cp = " + pprint.apply(cp))
-                System.err.println("processedCp = " + pprint.apply(processedCp))
-                printedCp = true
-              }
-              processedCp match {
+              process(cp).getOrElse(AggregateClassPath(Nil)) match {
                 case agg: AggregateClassPath =>
                   AggregateClassPath(modCp +: agg.aggregates)
                 case other =>
