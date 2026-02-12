1354841b030c2e71d666533a5d1303902e31590b
Alex Archambault <alexandre.archambault@gmail.com>
2026-02-12 19:26:38 +0100

    Fix unclosed progress reporting

--- presentation-compiler/src/main/dotty/tools/pc/CachingDriver.scala
+++ presentation-compiler/src/main/dotty/tools/pc/CachingDriver.scala
@@ -239,9 +239,11 @@ class CachingDriver(
           ""
       for (f <- compilerAccess.beforeAccessOpt)
         f(id, "typechecking", uri.toASCIIString)
-      previousDiags = super.run(uri, source)
-      for (f <- compilerAccess.afterAccessOpt)
-        f(id, "typechecking", uri.toASCIIString)
+      try previousDiags = super.run(uri, source)
+      finally {
+        for (f <- compilerAccess.afterAccessOpt)
+          f(id, "typechecking", uri.toASCIIString)
+      }
     }
     lastCompiledURI = uri
     previousDiags
