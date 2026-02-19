fe4246db07787ebfd57da32f5be9f6e78653bec0
Alex Archambault <alexandre.archambault@gmail.com>
2026-02-19 19:58:23 +0100

    Add debug mode

--- presentation-compiler/src/main/dotty/tools/pc/ScalaPresentationCompiler.scala
+++ presentation-compiler/src/main/dotty/tools/pc/ScalaPresentationCompiler.scala
@@ -78,6 +78,7 @@ class ScalaPresentationCompiler(
     PcConvertToNamedLambdaParameters.codeActionId,
   ).asJava
 
+  var debug: Boolean = false
   private val userLogger = userLoggerSupplier.get()
 
   val scalaVersion = BuildInfo.scalaVersion
@@ -246,7 +247,7 @@ class ScalaPresentationCompiler(
       params.uri.toASCIIString
     ) { access =>
       val driver = access.compiler()
-      PcDefinitionProvider(driver, params, search).definitions(module)
+      PcDefinitionProvider(driver, params, search, userLogger, debug).definitions(module)
     }(params.toQueryContext)
 
   override def typeDefinition(
@@ -259,7 +260,7 @@ class ScalaPresentationCompiler(
       params.uri.toASCIIString
     ) { access =>
       val driver = access.compiler()
-      PcDefinitionProvider(driver, params, search).typeDefinitions(module)
+      PcDefinitionProvider(driver, params, search, userLogger, debug).typeDefinitions(module)
     }(params.toQueryContext)
 
   def documentHighlight(
