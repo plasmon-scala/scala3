99733e0024fb4d3c0effa46d7928116157d70ef0
Alex Archambault <alexandre.archambault@gmail.com>
2026-03-06 13:38:51 +0100

    Allow users to force symbol index use in some cases

--- presentation-compiler/src/main/dotty/tools/pc/ScalaPresentationCompiler.scala
+++ presentation-compiler/src/main/dotty/tools/pc/ScalaPresentationCompiler.scala
@@ -64,7 +64,9 @@ class ScalaPresentationCompiler(
     var folderPath: Option[Path] = None,
     var reportsLevel: ReportLevel = ReportLevel.Info,
     var completionItemPriority: CompletionItemPriority = (_: String) => 0,
-    var reportContext: ReportContext = EmptyReportContext()
+    var reportContext: ReportContext = EmptyReportContext(),
+    // FIXME We need to pass this one around in more places and use it there
+    var preferSymbolSearch: String => Boolean = _ => false
 ) extends PresentationCompiler with HasCompilerAccess:
 
   override def buildTargetId(): String =
@@ -253,7 +255,7 @@ class ScalaPresentationCompiler(
       params.uri.toASCIIString
     ) { access =>
       val driver = access.compiler()
-      PcDefinitionProvider(driver, params, search, userLogger, debug).definitions(module)
+      PcDefinitionProvider(driver, params, search, userLogger, preferSymbolSearch, debug).definitions(module)
     }(params.toQueryContext)
 
   override def typeDefinition(
@@ -266,7 +268,7 @@ class ScalaPresentationCompiler(
       params.uri.toASCIIString
     ) { access =>
       val driver = access.compiler()
-      PcDefinitionProvider(driver, params, search, userLogger, debug).typeDefinitions(module)
+      PcDefinitionProvider(driver, params, search, userLogger, preferSymbolSearch, debug).typeDefinitions(module)
     }(params.toQueryContext)
 
   def documentHighlight(
