6931c729fffb64914a30c20494c53a20b7d7a388
Alex Archambault <alexandre.archambault@gmail.com>
2026-03-18 16:49:05 +0100

    Report PC diagnostics via plasmon logger
    Rather than them going to stderr

--- presentation-compiler/src/main/dotty/tools/pc/CachingDriver.scala
+++ presentation-compiler/src/main/dotty/tools/pc/CachingDriver.scala
@@ -29,6 +29,8 @@ import dotty.tools.dotc.classpath.PackageEntry
 import dotty.tools.io.FileZipArchive
 import java.util.UUID
 import java.util.concurrent.ConcurrentHashMap
+import java.util.function.Consumer
+import dotty.tools.dotc.reporting.Reporter
 
 /** CachingDriver is a wrapper class that provides a compilation cache for
  *  InteractiveDriver. CachingDriver skips running compilation if
@@ -51,7 +53,8 @@ import java.util.concurrent.ConcurrentHashMap
 class CachingDriver(
     override val settings: List[String],
     javaHome: Path,
-    compilerAccess: Scala3CompilerAccess
+    compilerAccess: Scala3CompilerAccess,
+    userLogger: Consumer[String]
 ) extends InteractiveDriver(settings):
 
   private var lastCompiledURI: URI = uninitialized
@@ -207,7 +210,16 @@ class CachingDriver(
           }
       }
     }
-    baseCtx.initialCtx.withProperty(Comments.ContextDoc, Some(new Comments.ContextDocstrings))
+    baseCtx.initialCtx
+      .withProperty(Comments.ContextDoc, Some(new Comments.ContextDocstrings))
+      .fresh
+      .setReporter(
+        new Reporter {
+          def doReport(dia: Diagnostic)(using Context): Unit = {
+            userLogger.accept(dia.toString)
+          }
+        }
+      )
   }
 
   private def alreadyCompiled(uri: URI, content: Array[Char]): Boolean =
